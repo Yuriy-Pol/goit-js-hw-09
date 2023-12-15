9c375cb904c30ccc256389c583f92d4af9dddce5
Yuriy-Pol <polozhevetsyura@gmail.com>
2023-12-15 23:49:42 +0200

    js

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -1,42 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
   const form = document.querySelector('.feedback-form');
 
-  // Перевірка стану сховища при завантаженні сторінки
-  const storedState = localStorage.getItem('feedback-form-state');
-  if (storedState) {
-    const { email, message } = JSON.parse(storedState);
-    form.querySelector('[name="email"]').value = email;
-    form.querySelector('[name="message"]').value = message;
-  }
+  const savedData =
+    JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+  form.elements.email.value = savedData.email || '';
+  form.elements.message.value = savedData.message || '';
 
-  // Відстеження події input на формі з використанням делегування
   form.addEventListener('input', function (event) {
-    if (
-      event.target.tagName === 'INPUT' ||
-      event.target.tagName === 'TEXTAREA'
-    ) {
-      // Запис значень полів у локальне сховище
+    if (event.target.name === 'email' || event.target.name === 'message') {
       const currentState = {
-        email: form.querySelector('[name="email"]').value,
-        message: form.querySelector('[name="message"]').value,
+        email: form.elements.email.value,
+        message: form.elements.message.value,
       };
       localStorage.setItem('feedback-form-state', JSON.stringify(currentState));
     }
   });
 
-  // Обробник події submit форми
   form.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    // Очищення сховища та полів форми
+    const currentState = {
+      email: form.elements.email.value,
+      message: form.elements.message.value,
+    };
+
+    console.log(currentState);
+
     localStorage.removeItem('feedback-form-state');
     form.reset();
-
-    // Вивід у консоль об'єкту з полями email та message та їхніми поточними значеннями
-    const submittedData = {
-      email: currentState.email,
-      message: currentState.message,
-    };
-    console.log(submittedData);
   });
 });
